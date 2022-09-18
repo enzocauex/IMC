b5a74cc69a000b30b485b8974375b0603054f9c0
Enzo Soares <enzocaue93@gmail.com>
2022-09-18 13:33:58 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
 import { Modal } from './modal.js'
+import { alertError } from './alert-error.js'
+import { calculateIMC, notNumber } from './utils.js'
 
-//variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
 // quando a arrow function recebe apenas um argumento, ela pode ser escrita sem os parênteses
+inputHeight.oninput = () => alertError.close()
+inputHeight.oninput = () => alertError.close()
 form.onsubmit = event => { 
     event.preventDefault()
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const showAlertError = notNumber(weight) || notNumber(height)
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
 
-    if(showAlertError) {
-        console.log('mostrar o alerta de erro')
+    if(weightOrHeightIsNotANumber) {
+        alertError.open()
         return;
     }
 
-    const result = IMC(weight, height)
+    alertError.close()
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
     Modal.open()
 }
-
-//functions
-function notNumber(value) {
-    return isNaN(value) || value == ""
-}
-
-function IMC(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
-  }
